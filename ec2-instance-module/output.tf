05a3ddcf09ee73f54c79688765b92963e05a29cd
siva <siva@example.com>
2025-04-11 21:54:09 +0530

    changes are made as per errors in actions

--- ec2-instance-module/output.tf
+++ ec2-instance-module/output.tf
@@ -11,32 +11,62 @@
 
 
 
-output "vpc_id" {
-  value = module.vpc_module.vpc_id
+# output "vpc_id" {
+#   value = module.vpc_module.vpc_id
+# }
+
+# output "public_subnet_id" {
+#   value = module.vpc_module.public_subnet_id
+# }
+
+# output "private_subnet_id" {
+#   value = module.vpc_module.private_subnet_id
+# }
+
+# output "igw_id" {
+#   value = module.vpc_module.igw_id
+# }
+
+
+
+# # outputs.tf in the root module
+
+# output "ec2_instance_ids" {
+#   description = "The IDs of the EC2 instances from the module"
+#   value       = module.vpc_module.ec2_instance_ids
+# }
+
+# output "ec2_public_ips" {
+#   description = "The public IP addresses of the EC2 instances from the module"
+#   value       = module.vpc_module.ec2_public_ips
+# }
+
+output "vpc_ids" {
+  description = "List of VPC IDs from all module instances"
+  value       = [for m in module.vpc_module : m.vpc_id]
 }
 
-output "public_subnet_id" {
-  value = module.vpc_module.public_subnet_id
+output "public_subnet_ids" {
+  description = "List of public subnet IDs from all module instances"
+  value       = [for m in module.vpc_module : m.public_subnet_id]
 }
 
-output "private_subnet_id" {
-  value = module.vpc_module.private_subnet_id
+output "private_subnet_ids" {
+  description = "List of private subnet IDs from all module instances"
+  value       = [for m in module.vpc_module : m.private_subnet_id]
 }
 
-output "igw_id" {
-  value = module.vpc_module.igw_id
+output "igw_ids" {
+  description = "List of Internet Gateway IDs from all module instances"
+  value       = [for m in module.vpc_module : m.igw_id]
 }
-
-
-
-# outputs.tf in the root module
 
 output "ec2_instance_ids" {
-  description = "The IDs of the EC2 instances from the module"
-  value       = module.vpc_module.ec2_instance_ids
+  description = "List of EC2 instance IDs from all module instances"
+  value       = [for m in module.vpc_module : m.ec2_instance_ids]
 }
 
 output "ec2_public_ips" {
-  description = "The public IP addresses of the EC2 instances from the module"
-  value       = module.vpc_module.ec2_public_ips
+  description = "List of public IPs from all module instances"
+  value       = [for m in module.vpc_module : m.ec2_public_ips]
 }
