38d12bbc0676cd7bc0b36334c78165a5d3ad3e12
siva <siva@example.com>
2025-04-14 22:14:21 +0530

    added new dir for kubernates

--- ec2-instance-module/minikube.sh
+++ ec2-instance-module/minikube.sh
@@ -1,23 +1,28 @@
+
+#!/bin/bash
+# Sample Soanrqube
+exec > /var/log/user-data.log 2>&1
+set -xe
+
 # Install Docker
 sudo apt update && sudo apt -y install docker.io
 
  #Install kubectl
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.7/bin/linux/amd64/kubectl && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.7/bin/linux/amd64/kubectl && sudo chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
 
 # Install Minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.23.2/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.23.2/minikube-linux-amd64 && sudo chmod +x minikube && sudo mv minikube /usr/local/bin/
 
  # Start Minikube
 sudo apt install conntrack
-minikube start --vm-driver=none
+sudo minikube start --vm-driver=none
 
 #  Install kubectl
 
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}' 
+sudo kubectl create namespace argocd
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sudo kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}' 
 
-# For version 1.9 or later:
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
+
 
